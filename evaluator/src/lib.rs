6d5052911d865e14b62c67491c159559d293b874
Álvaro <ahdezex@gmail.com>
2023-09-26 20:07:31 +0200

    bang and minus prefixes

--- evaluator/src/lib.rs
+++ evaluator/src/lib.rs
@@ -18,7 +18,15 @@ fn eval(node: Node) -> object::Object {
             ast::Expression::Identifier(_) => todo!(),
             ast::Expression::StringLiteral(_) => todo!(),
             ast::Expression::IntegerLiteral(i) => Object::Integer(i),
-            ast::Expression::Prefix(_) => todo!(),
+            ast::Expression::Prefix(prefix) => {
+                let right = eval(ast::Node::Expression(*prefix.right));
+
+                match prefix.operator.as_str() {
+                    "!" => evalBangOperatorExpression(right),
+                    "-" => evalMinusPrefixOperatorExpression(right),
+                    _ => Object::Null,
+                }
+            }
             ast::Expression::Infix(_) => todo!(),
             ast::Expression::Empty => todo!(),
             ast::Expression::Boolean(boolean) => Object::Boolean(boolean),
@@ -39,6 +47,22 @@ fn evalStatements(statements: Vec<ast::Statement>) -> object::Object {
     result
 }
 
+fn evalBangOperatorExpression(right: object::Object) -> object::Object {
+    match right {
+        object::Object::Boolean(true) => object::Object::Boolean(false),
+        object::Object::Boolean(false) => object::Object::Boolean(true),
+        object::Object::Null => object::Object::Boolean(true),
+        _ => object::Object::Boolean(false),
+    }
+}
+
+fn evalMinusPrefixOperatorExpression(right: object::Object) -> object::Object {
+    match right {
+        object::Object::Integer(i) => object::Object::Integer(-i),
+        _ => object::Object::Null,
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use anyhow::Result;
@@ -47,7 +71,7 @@ mod tests {
 
     #[test]
     fn test_integer_expression() -> Result<()> {
-        let tests = vec![("5", 5), ("10", 10)];
+        let tests = vec![("5", 5), ("10", 10), ("-5", -5), ("-10", -10)];
 
         for (input, expected) in tests {
             let lexer = lexer::Lexer::new(input.into());
@@ -65,6 +89,30 @@ mod tests {
     #[test]
     fn test_boolean_literals() -> Result<()> {
         let tests = vec![("true", true), ("false", false)];
+
+        for (input, expected) in tests {
+            let lexer = lexer::Lexer::new(input.into());
+            let mut parser = parser::Parser::new(lexer)?;
+            let program = parser.parse_program()?;
+
+            let evaluation = eval(ast::Node::Program(program));
+
+            assert_eq!(evaluation, object::Object::Boolean(expected));
+        }
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_bang_prefix() -> Result<()> {
+        let tests = vec![
+            ("!true", false),
+            ("!false", true),
+            ("!5", false),
+            ("!!true", true),
+            ("!!false", false),
+            ("!!5", true),
+        ];
 
         for (input, expected) in tests {
             let lexer = lexer::Lexer::new(input.into());
